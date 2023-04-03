54a17875eaffecc3545c5d6e0ac3fc3ddd097076
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2023-04-03 11:30:39 +0300

    step1

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -6,3 +6,66 @@
 // TODO: add function to update a user
 // TODO: add function to delete a user
 // TODO: add function to check if a token is valid
+
+import {NextFunction, Request, Response} from 'express';
+import userModel from '../models/userModel';
+import CustomError from '../../classes/CustomError';
+import {User} from '../../interfaces/User';
+import bcrypt from 'bcryptjs';
+import DBMessageResponse from '../../interfaces/DBMessageResponse';
+
+const salt = bcrypt.genSaltSync(12);
+
+const check = (req: Request, res: Response) => {
+  res.json({message: 'I am alive'});
+};
+
+const userListGet = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await userModel.find().select('-password -role');
+    res.json(users);
+  } catch (error) {
+    next(new CustomError((error as Error).message, 500));
+  }
+};
+
+const userGet = async (
+  req: Request<{id: string}>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await userModel
+      .findById(req.params.id)
+      .select('-password -role');
+    if (!user) {
+      next(new CustomError('User not found', 404));
+    }
+    res.json(user);
+  } catch (error) {
+    next(new CustomError((error as Error).message, 500));
+  }
+};
+
+const userPost = async (
+  req: Request<{}, {}, User>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = req.body;
+    user.password = await bcrypt.hash(user.password, salt);
+    const newUser = await userModel.create(user);
+    const response: DBMessageResponse = {
+      message: 'user created',
+      user: {
+        user_name: newUser.user_name,
+        email: newUser.email,
+        id: newUser._id,
+      },
+    };
+    res.json(response);
+  } catch (error) {
+    next(new CustomError((error as Error).message, 500));
+  }
+};
